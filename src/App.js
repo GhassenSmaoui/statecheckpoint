4e6466bf7f0c27267796fffcb19cae6b0634ed24
GhassenSmaoui <smaouighassen@yahoo.fr>
2020-12-23 09:39:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Card } from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullName: "Mohamed ben Mohamed",
+      bio: "I love teaching",
+      imgSrc: "./myPhoto.png",
+      profession: "Teacher",
+      isShow: false,
+      sec: 0,
+    };
+  }
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        sec: this.state.sec + 1,
+      });
+    }, 1000);
+  }
+
+  showProfile = () => {
+    this.setState({
+      isShow: !this.state.isShow,
+    });
+  };
+
+  displayTime = (timeSec) => {
+    let h = Math.floor(timeSec / 3600);
+    let m = Math.floor((timeSec - h * 3600) / 60);
+    let s = timeSec - h * 3600 - m * 60;
+    return (
+      <div>
+        <span>
+          {String(h).padStart(2, 0)} : {String(m).padStart(2, 0)} :{" "}
+          {String(s).padStart(2, 0)}
+        </span>
+      </div>
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <p> {this.displayTime(this.state.sec)}</p>
+        <button
+          onClick={this.showProfile}
+          /*className={this.state.isShow ? "red" : "green"} */
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          {this.state.isShow ? "hide profile" : "display profile"}
+        </button>
 
-export default App;
+        {this.state.isShow ? (
+          <div>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={this.state.imgSrc} />
+              <Card.Body>
+                <Card.Title> {this.state.fullName} </Card.Title>
+                <Card.Text>
+                  <h2> I work as a {this.state.profession}</h2>
+                  {this.state.bio}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+        ) : (
+          <h1>click on the button to show the profile</h1>
+        )}
+      </div>
+    );
+  }
+}
